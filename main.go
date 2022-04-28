d9e9597c443c054ab9ed01371add1ecea85ab4be
Dag5T <unmeinoya@gmail.com>
2022-04-28 13:17:03 +0200

    podlaczanie packagea

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
-import fmt
 
-function main(){
+import (
+	"fmt"
+	"fractions/fractions"
+)
+
+func main() {
+	testFraction := fractions.Construct(2, 4)
+	testFraction.Display()
 	fmt.Println("OK")
 }
